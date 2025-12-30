6742d354350fd53d7af07cc6913cdeaa5f82048f
Kosty777 <kostenkodnipro@gmail.com>
2025-12-31 00:28:03 +0200

    31.12.2025 0:27 did hub started level 2

--- Assets/HubScene/Scripts/Professor/ProfessorTrigger.cs
+++ Assets/HubScene/Scripts/Professor/ProfessorTrigger.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class ProfessorTrigger : MonoBehaviour
 {
+    // Посилання на професора
     public ProfessorWalker professor;
+
+    // Чи спрацьовує тригер тільки один раз
     public bool oneTime = true;
 
     private bool triggered = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Якщо вже спрацювало і потрібно тільки один раз
         if (triggered && oneTime) return;
 
+        // Перевіряємо, що торкнувся гравець
         if (other.CompareTag("Player"))
         {
             triggered = true;
+
+            // Запускаємо поточний сегмент професора
             professor.StartCurrentSegmentExternally();
         }
     }
